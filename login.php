b536bbffb52f78b8538fc1a7632355d6ba4f9001
MFahrezii <trezi069@gmail.com>
2024-05-28 11:59:43 +0700

    p

--- login.php
+++ login.php
@@ -1,26 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../assets/css/login.css">
-    <link rel="stylesheet" href="assets/login.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../assets/css/style.css">
+  <link rel="stylesheet" href="assets/login.css">
+  <title>Login Form</title>
 </head>
 <body>
-<div class="login-container">
-        <h2>Login</h2>
-        <form>
-            <div class="input-group">
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username" required>
-            </div>
-            <div class="input-group">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
-            </div>
-            <button type="submit">Login</button>
-        </form>
-    </div>
+  <div class="login-container">
+    <form action="../../" method="post">
+      <h2>Login</h2>
+      <label for="username">Username:</label>
+      <input type="text" id="username" name="username" required autocomplete="on">
+      
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" required autocomplete="on">
+      <button type="submit">Login</button>
+    
+    </form>
+  </div>
 </body>
 </html>
